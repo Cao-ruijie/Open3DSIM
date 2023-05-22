37467638d0cb564284a15a73ec2e300e3176371c
Cao-ruijie <112751580+Cao-ruijie@users.noreply.github.com>
2023-05-22 15:37:45 +0800

    Update get_vectormodelOTF.m

--- MATLAB_Open_3DSIM/help_functions/get_vectormodelOTF.m
+++ MATLAB_Open_3DSIM/help_functions/get_vectormodelOTF.m
@@ -1,5 +1,7 @@
 function [OTFinc,OTFinc2d_throughfocus] = get_vectormodelOTF(OTFparams)
-
+  
+% copyright Sjoerd Stallinga TUD 2017-2020
+  
 %% Get pupil matrix
 [~,~,wavevector,wavevectorzmed,~,PupilMatrix] = get_pupil_matrix(OTFparams);
 
